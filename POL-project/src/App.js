5059528c61a93cf898c321083fc7cc54d38c3dd6
JeongMinSu <wjdalstn2752@naver.com>
2024-07-06 15:59:30 +0900

    change

--- POL-project/src/App.js
+++ POL-project/src/App.js
@@ -1,23 +1,38 @@
 import React from 'react';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import Login from './Login';
-import Signup from './Signup';
-import Home from './Home';
-import StudySetup from './StudySetup'; // 추가된 StudySetup 컴포넌트
+import { Route, Routes, Link } from 'react-router-dom';
+import Main from './components/Main';
+import Learn from './components/Learn';
+import Speak from './components/Speak';
+import Listen from './components/Listen';
+import Settings from './components/Settings';
 import './App.css';
+import logo from './char.png'; 
 
 function App() {
   return (
-    <Router>
-      <div className="App">
-        <Routes>
-          <Route path="/" element={<Login />} />
-          <Route path="/signup" element={<Signup />} />
-          <Route path="/home" element={<Home />} />
-          <Route path="/study-setup" element={<StudySetup />} />
-        </Routes>
+    <div className="App">
+      <div style={{ display: 'flex' }}>
+        <nav>
+            <img src={logo} alt="로고" className="logo" />
+          <ul>
+            <li><Link to="/">메인 화면</Link></li>
+            <li><Link to="/learn">배우기</Link></li>
+            <li><Link to="/speak">말하기</Link></li>
+            <li><Link to="/listen">듣기</Link></li>
+            <li><Link to="/settings">설정</Link></li>
+          </ul>
+        </nav>
+        <main>
+          <Routes>
+            <Route path="/" element={<Main />} />
+            <Route path="/learn" element={<Learn />} />
+            <Route path="/speak" element={<Speak />} />
+            <Route path="/listen" element={<Listen />} />
+            <Route path="/settings" element={<Settings />} />
+          </Routes>
+        </main>
       </div>
-    </Router>
+    </div>
   );
 }
 
